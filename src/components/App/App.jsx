9a47761430a37dfa0a7b6622d5fc4d71dee129f6
Kristina <kr.hranovska@gmail.com>
2024-02-25 13:52:45 +0200

    fist commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import Contact from "../Contact/Contact";
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+
+function App() {
+  return (
+    <>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
